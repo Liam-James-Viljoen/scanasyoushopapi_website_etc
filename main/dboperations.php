afcec8ef3b8e1a2057277822292b78bfd6ecfda8
Liamj <liamjames2@hotmail.co.uk>
2019-01-22 00:50:50 +0000

    commands added

--- main/dboperations.php
+++ main/dboperations.php
@@ -12,6 +12,29 @@ class DBOperations{
     //by calling the method connect of DB_connection class
     $this->connection = $db->connect();
   }
-  
+  function createuser($username, $password, $email, $phonenumber){
+    $stmt=$this->$connection->prepare('INSERT INTO Users (username, password, email, phonenumber) VALUES (?, ?, ?, ?)');
+    $stmt->bind_param("ssis", $username, $password, $email, $phonenumber);
+    if ($stmt->execute()){
+      return true;
+      return false;
+    }
+  }
+  function selectuser(){
+    $stmt=$this->$connection->prepare('SELECT id, username, password FROM users');
+    $stmt->execute();
+    $stmt->bind_param($id, $username, $password);
+
+    $users = array();
+    while($stmt->fetch()){
+      $user = array();
+      $user['id'] = id;
+      $user['username'] = $username;
+      $user['password'] = $password;
+
+      array_push($users, $user);
+    }
+    return users;
+  }
 }
 ?>
